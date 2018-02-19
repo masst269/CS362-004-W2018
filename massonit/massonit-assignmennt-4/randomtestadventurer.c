5fdd6444c9771cb5bdbcfc1ae98c74c595e3ff7a
masst269 <massonit@oregonstate.edu>
2018-02-18 22:32:31 -0800

    Update randomtestadventurer.c

--- massonit/massonit-assignmennt-4/randomtestadventurer.c
+++ massonit/massonit-assignmennt-4/randomtestadventurer.c
@@ -14,6 +14,8 @@ int main(){
 		int i, j, m, players, seed, player = 0, cost = 0, count = 0, count1 = 0, count2 = 0, count3 = 0, failures = 0, fail = 0, success = 0;
 		struct gameState G;
 		srand(time(0));
+		printf("Random Testing for the Adventurer\n");
+		//The randomness comes the generating numbers for the deck, hand, discard, and player count
 		for( i = 0, failures = 0, success = 0; i < NUM_TEST; i++){
 			players = rand() % 4;
 			seed = rand();
@@ -61,7 +63,7 @@ int main(){
 			}
 			printf("Completed Test #%d\n", i);
 		}
-		printf("`\n");
+		printf("\n");
 		printf("State: %d failures and %d sucesses\n", failures, success);
 		printf("%d game run\n", NUM_TEST);
 		return 0;
